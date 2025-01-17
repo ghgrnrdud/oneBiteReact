fedfeaa634a5b8b47b2543a04f1a0cb3890135f2
minseo <rnrdudghg3122@gmail.com>
2025-01-17 16:54:03 +0900

    first commit

---
+++ section01/chapter04.js
@@ -0,0 +1,21 @@
+// 1. 변수
+let age 
+console.log(age); // 27
+
+age = 30;
+console.log(age); // 30
+
+// 2. 상수(변하지않는 값/초기화 o)
+const birth = "1999.03.23";
+birth = "123";
+
+// 3. 변수 명명규칙(네이밍 규칙)
+// 3-1. $, _ 제외한 기호는 사용 x
+let s_name;
+
+// 3-2. 숫자로 시작할 수 없다.
+let name1;
+let _2name; 
+
+// 3-3. 예약어를 사용할 수 없다.
+// let if;
---
+++ section05/src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Main from './components/Main.jsx';
+import Button from './components/Button.jsx';
+function App() {
+
+  return (
+    <>
+    {/* <Header />
+    <Main></Main>
+    <Footer /> */}
+    <Button text={"메일"} color={"red"}/>
+    <Button text={"블로그"} color={"orange"}/>
+    <Button text={"카페"}>
+      <div>자식요소</div>
+      <Header></Header>
+    </Button>
+    </>
+  );
+}
+
+export default App
